3dd4afc7322c4ee5f87c64666bee8f00fb4c198d
jidanji <42198953+jidanji@users.noreply.github.com>
2020-08-31 17:17:39 +0800

    在”master“提交

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div id="mainTitle">银河对接项目</div>
+    <router-view />
   </div>
 </template>
 
 <style lang="stylus">
+#mainTitle {
+  background: #000000;
+  color: #ffffff;
+  text-align: left;
+  height: 50px;
+  line-height: 50px;
+  font-size: 18px;
+  padding-left: 10px;
+  letter-spacing: 2px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 999;
+}
 </style>
